c4bff6ebdccb525dec4b0a8000edc53f8b32e4aa
Jonathan Gamble <jdgamble555@gmail.com>
2023-08-26 17:13:15 -0500

    test

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,12 +3,14 @@ import { revalidatePath } from 'next/cache';
 
 export async function GET(request: NextRequest) {
 
+    const r = request.headers.get('referer') || '';
+
     /*
     const secret = request.nextUrl.searchParams.get('secret');
     if (secret !== process.env.BYPASS_TOKEN) {
         return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
     }
     */
-    revalidatePath('/');
+    revalidatePath(r);
     return NextResponse.json({ revalidated: true, now: Date.now() });
 }
