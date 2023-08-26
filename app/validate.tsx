4e4dde18a3a84dbf6e3e7b200dd7253e8098dfa2
Jonathan Gamble <jdgamble555@gmail.com>
2023-08-26 16:11:07 -0500

    invalidate

--- app/validate.tsx
+++ app/validate.tsx
@@ -8,7 +8,13 @@ export function Validate() {
 
     return (
         <>
-            <button type="submit" onClick={() => fetch(window.location.origin + '/api/revalidate').then(() => setInvalidated(true))}>
+            <button type="submit" onClick={() => fetch(window.location.origin + '/api/revalidate').then(async (r) => {
+                const x = await r.json();
+                if (x.revalidated) {
+                    setInvalidated(true);
+                }
+            }
+            )}>
                 Invalidate
             </button>
             <button onClick={() => window.location.reload()}>Refresh</button>
