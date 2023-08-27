02960ce2f9fac03e7583a2b8387317b3f2cd29f8
Jonathan Gamble <jdgamble555@gmail.com>
2023-08-27 14:02:55 -0500

    test3

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
 export async function GET() {
-    revalidatePath('/');
+    revalidatePath('/page');
     return NextResponse.json({ revalidated: true, now: Date.now() });
 }
