64a9aec1157e9ad5805a4b98d558bb49192a9135
Jonathan Gamble <jdgamble555@gmail.com>
2023-09-01 20:46:11 -0500

    test

--- app/revalidate/page.tsx
+++ app/revalidate/page.tsx
@@ -1,6 +1,8 @@
 import { revalidatePath } from "next/cache";
 import { BackButton } from "../back-button";
 
+export const dynamic = 'force-dynamic';
+
 export default async function revalidate() {
     revalidatePath('/');
     return (
